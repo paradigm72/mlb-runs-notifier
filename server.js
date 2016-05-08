c8311584df0e78e368577bf276c88f57af8dfdf1
Paul Romine <promine@gmail.com>
2016-05-07 21:49:55 -0500

    retrieve notifications and chain them back to the client

--- server.js
+++ server.js
@@ -1,42 +1,55 @@
 var http = require("http");
 var express = require("express");
 var bodyParser = require("body-parser");
-//const MLBBoxScores = require('mlbboxscores');
 
 var app = express();
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing
-var mlbOptions = { path: 'year_2011/month_07/day_23'};
 
+MLBhost = 'gd2.mlb.com'
+baseURL = '/components/game/mlb/'
+todayPath = 'year_2016/month_05/day_07/'
+gamePath = 'gid_2016_05_07_texmlb_detmlb_1/'
+notificationURL = 'notifications/notifications_full.xml'
+composedURL = baseURL + todayPath + gamePath + notificationURL
 
-//Test basic HTTP connectivity
+//Retrieve notification log for this game
 http.createServer(function(request, response) {
 	console.log("GET Request on port 8888");
     response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
 	response.writeHead(200, {"Content-Type": "text/plain"});
+	var parentResponse = response;
 
-	//var mlbboxscores = new MLBBoxScores(mlbOptions);
-	//mlbboxscores.get(function(err, boxscores) {
-	//	console.log(boxscores);
-	//});
+	var options = {
+		host: MLBhost,
+		path: composedURL
+	};
+	var notificationList;
 
-	response.write("Hello World");
-	response.end();
+	callback = function(response) {
+		response.on('data', function(chunk) {
+			notificationList += chunk;
+		});
+
+		response.on('end', function() {
+			console.log(notificationList);
+			parentResponse.write(notificationList);
+			parentResponse.end();
+		});
+	}
+	console.log("End of response");
+    http.request(options, callback).end();	
+
 }).listen(8888);   
 
 
 
 app.get('/getResponder',function(request, response) {
-    //request.setHeader("Access-Control-Allow-Origin", "*");
-    //request.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
 	console.log("GET Request on port 8000");
-	//var value = request.value;
 	var body = request.body;
 	console.log(body);
 	console.log(request);
-    //console.log(value);
-	//console.log(input.value);
     response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
 	response.writeHead(200, {"Content-Type": "text/plain"});
@@ -44,22 +57,7 @@ app.get('/getResponder',function(request, response) {
     response.end();
 });
 
-app.post('/postResponder',function(request, response) {
-	console.log("POST Request on port 8000");
-	//var value = request.value;
-	//var params = request.params;
-	var body = request.body;
-	console.log(body);
-	//console.log(params);
-	console.log(request);
-    //console.log(value);
-	//console.log(input.value);
-    response.setHeader("Access-Control-Allow-Origin", "*");
-    response.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write('you told us' + request.value);
-    response.end();
-});
+
 
 app.listen(8000);
 
